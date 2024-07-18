69946c295a47952c764b1336264803ac0e54c87d
Julian Ranieri <68655515+royalsaltmerchant@users.noreply.github.com>
2024-07-17 23:54:49 -0700

    feafeaf

--- commands/start_dev.sh
+++ commands/start_dev.sh
@@ -2,7 +2,7 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-# echo "\n*************** RUNNING AUTO MIGRATE ***************\n"
+echo "\n*************** RUNNING AUTO MIGRATE ***************\n"
 npm run migrate:up
 echo "\n*************** RUNNING TYPESCRIPT ***************\n"
 npx tsc --watch &
