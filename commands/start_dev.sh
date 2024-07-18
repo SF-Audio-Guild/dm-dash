6628e1e259e5d274e552cc725035a6adbdac8dc0
Julian Ranieri <68655515+royalsaltmerchant@users.noreply.github.com>
2024-07-17 23:53:46 -0700

    undo comment out start script thing

--- commands/start_dev.sh
+++ commands/start_dev.sh
@@ -3,7 +3,7 @@ trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
 # echo "\n*************** RUNNING AUTO MIGRATE ***************\n"
-# npm run migrate:up
+npm run migrate:up
 echo "\n*************** RUNNING TYPESCRIPT ***************\n"
 npx tsc --watch &
 echo "\n*************** STARTING JS BUNDLER ***************\n"
